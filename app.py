36ab97683d88f48611b24a8afd888d18fa660fea
Edward Z. Yang <ezyang@cs.stanford.edu>
2014-12-31 20:03:07 -0500

    Plz to not truncate
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ class User(Base):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(64))
+    password = db.Column(db.String(100))
     realname = db.Column(db.String(100))
     email = db.Column(db.String(100))
 
